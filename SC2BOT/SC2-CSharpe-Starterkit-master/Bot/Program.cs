61163def7832e752955de182171cc8bad25e16cc
MAIN1 <rjg4242@yahoo.com>
2018-07-13 16:40:28 -0400

    init

---
+++ SC2BOT/SC2-CSharpe-Starterkit-master/Bot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using SC2APIProtocol;
+
+namespace Bot
+{
+    class Program
+    {
+        // Settings for your bot.
+        private static BotFactory botFactory = new RaxBotFactory();
+        private static Race race = Race.Terran;
+
+        // Settings for single player mode.
+//        private static string mapName = "AbyssalReefLE.SC2Map";
+//        private static string mapName = "AbiogenesisLE.SC2Map";
+        
+        private static string mapName = "(2)16-BitLE.SC2Map";
+//        private static string mapName = "FrostLE.SC2Map";
+        private static Race opponentRace = Race.Random;
+        private static Difficulty opponentDifficulty = Difficulty.VeryEasy;
+
+        public static GameConnection gc = null;
+        
+        static void Main(string[] args)
+        {
+            try {
+                gc = new GameConnection();
+                if (args.Length == 0)
+                    gc.RunSinglePlayer(botFactory, mapName, race, opponentRace, opponentDifficulty).Wait();
+                else {
+                    gc.RunLadder(botFactory, race, args).Wait();
+                }
+            }
+            catch (Exception ex) {
+                Logger.Info(ex.ToString());
+            }
+            
+            Logger.Info("Terminated.");
+        }
+    }
+}
---
+++ SC2BOT/SC2-CSharpe-Starterkit-master/Bot/Wrapper/Interface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SC2APIProtocol;
+
+namespace Bot
+{
+    public interface Bot
+    {
+        IEnumerable<SC2APIProtocol.Action> OnFrame(ResponseObservation observation, uint playerId);
+    }
+
+    public interface BotFactory
+    {
+        Bot GetBot(ResponseGameInfo gameInfo, ResponseData gameData);
+    }
+}
